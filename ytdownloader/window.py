892116c48be457d371c9bfaf3651788f81796dd7
Michał Rosiak <michal.rosiak.dev@gmail.com>
2017-10-18 11:08:55 +0200

    Progressbar in downloader

--- ytdownloader/window.py
+++ ytdownloader/window.py
@@ -2,10 +2,13 @@
 import gi
 import pafy
 import requests
+import threading
 
 from datetime import datetime
-gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, Gio, GdkPixbuf, GLib
+
+gi.require_version('Gtk', '3.0')
+
 import headerbar as hb
 import welcome as wl
 from downloads import Downloads
@@ -26,7 +29,7 @@ class Window(Gtk.Window):
         self.set_titlebar(self.hbar)
         
         self.video_window = Video()
-        self.video_window.download_video_button.connect("clicked", self.download_video)
+        self.video_window.download_video_button.connect("clicked", self.add_to_download_section)
 
         self.welcome = wl.Welcome()
         self.open_popup = self.welcome.light
@@ -34,7 +37,7 @@ class Window(Gtk.Window):
 
         self.add(self.welcome)
 
-    def open_download_screen(self, widget):
+    def open_download_screen(self, widget=None):
         if self.get_child() == self.download_screen:
             self.remove(self.get_child())
             self.add(self.current)
@@ -60,26 +63,38 @@ class Window(Gtk.Window):
         self.get_video()
 
     def mycb(self, total, recvd, ratio, rate, eta):
-        print(ratio)
+        self.download_progressbar.set_fraction(ratio)
 
-    def download_video(self, widget):
+    def download_video(self):
+        self.video_window.video.videostreams[self.video_window.video_combobox.get_active()].download(quiet=True, callback=self.mycb)
+
+    def add_to_download_section(self, widget):
         self.download_hbox = Gtk.HBox()
 
         self.image = Gtk.Image()
         
         self.download_label = Gtk.Label(self.video_window.video.title)
+        self.download_progressbar = Gtk.ProgressBar()
 
-        response=requests.get(self.video_window.video.bigthumb)
+        response=requests.get(self.video_window.video.thumb)
         input_stream = Gio.MemoryInputStream.new_from_bytes(GLib.Bytes.new(response.content))             
         pixbuf = GdkPixbuf.Pixbuf.new_from_stream(input_stream, None) 
         self.image.set_from_pixbuf(pixbuf)
 
         self.download_hbox.pack_start(self.image, False, False, 5)
-        self.download_hbox.pack_start(self.download_label, False, False, 5)
+        self.download_vbox = Gtk.VBox()
+        self.download_vbox.pack_start(self.download_label, False, False, 5)
+        self.download_vbox.pack_start(self.download_progressbar, False, False, 5)
+
+        self.download_hbox.pack_start(self.download_vbox, False, False, 5)
 
         self.download_screen.pack_start(self.download_hbox, False, False, 5)
         self.download_screen.show_all()
-        self.video_window.video.videostreams[self.video_window.video_combobox.get_active()].download(quiet=True, callback=self.mycb)
+
+        self.open_download_screen()
+
+        self.work_thread = threading.Thread(target=self.download_video)
+        self.work_thread.start()
 
     def get_video(self):
         if self.type == 0:
